73ed4592ca5323631aeeb428aa0e49ef946751d8
Jefry Morera <jefrymoreraespinoza@gmail.com>
2023-11-20 00:01:51 -0600

    working in the interface

--- Proyect2_IIIC/src/view/mainView.java
+++ Proyect2_IIIC/src/view/mainView.java
@@ -28,11 +28,23 @@ public class mainView extends javax.swing.JFrame {
         lblOptions = new javax.swing.JLabel();
         ProfilePane = new javax.swing.JPanel();
         jSeparator1 = new javax.swing.JSeparator();
+        lblUser = new javax.swing.JLabel();
+        lblProfile = new javax.swing.JLabel();
         SerchPane = new javax.swing.JPanel();
         jSeparator2 = new javax.swing.JSeparator();
+        lblSerch = new javax.swing.JLabel();
+        lblSerchEvent = new javax.swing.JLabel();
         BookingPane = new javax.swing.JPanel();
         jSeparator3 = new javax.swing.JSeparator();
+        lblCalendar = new javax.swing.JLabel();
+        lblBooking = new javax.swing.JLabel();
+        separator1 = new javax.swing.JSeparator();
+        separator2 = new javax.swing.JSeparator();
         lblOptions1 = new javax.swing.JLabel();
+        Events = new javax.swing.JPanel();
+        pnBooking = new javax.swing.JPanel();
+        pnProfile = new javax.swing.JPanel();
+        pnSerchEvent = new javax.swing.JPanel();
         lblIcon = new javax.swing.JLabel();
 
         jMenu3.setText("jMenu3");
@@ -69,6 +81,9 @@ public class mainView extends javax.swing.JFrame {
 
         ProfilePane.setBackground(new java.awt.Color(211, 82, 1));
         ProfilePane.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                ProfilePaneMouseClicked(evt);
+            }
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 ProfilePaneMouseEntered(evt);
             }
@@ -81,10 +96,22 @@ public class mainView extends javax.swing.JFrame {
         jSeparator1.setBackground(new java.awt.Color(0, 0, 0));
         ProfilePane.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 60, 189, 10));
 
-        SideBar.add(ProfilePane, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 180, 190, 60));
+        lblUser.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/images/user.png"))); // NOI18N
+        ProfilePane.add(lblUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 48, 48));
+
+        lblProfile.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        lblProfile.setForeground(new java.awt.Color(0, 0, 0));
+        lblProfile.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblProfile.setText("Mi perfil");
+        ProfilePane.add(lblProfile, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 10, 140, 40));
+
+        SideBar.add(ProfilePane, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 210, 190, 60));
 
         SerchPane.setBackground(new java.awt.Color(211, 82, 1));
         SerchPane.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                SerchPaneMouseClicked(evt);
+            }
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 SerchPaneMouseEntered(evt);
             }
@@ -96,6 +123,15 @@ public class mainView extends javax.swing.JFrame {
 
         jSeparator2.setBackground(new java.awt.Color(0, 0, 0));
         SerchPane.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 60, 189, 10));
+
+        lblSerch.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/images/serch.png"))); // NOI18N
+        SerchPane.add(lblSerch, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 48, 48));
+
+        lblSerchEvent.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        lblSerchEvent.setForeground(new java.awt.Color(0, 0, 0));
+        lblSerchEvent.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblSerchEvent.setText("Buscar Evento");
+        SerchPane.add(lblSerchEvent, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 10, 140, 40));
 
         SideBar.add(SerchPane, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 60, 190, 60));
 
@@ -113,9 +149,26 @@ public class mainView extends javax.swing.JFrame {
         jSeparator3.setBackground(new java.awt.Color(0, 0, 0));
         BookingPane.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 60, 189, 10));
 
-        SideBar.add(BookingPane, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 120, 190, 60));
+        lblCalendar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/images/calendar.png"))); // NOI18N
+        BookingPane.add(lblCalendar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 48, 48));
 
-        DashBoard.add(SideBar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 540));
+        lblBooking.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        lblBooking.setForeground(new java.awt.Color(0, 0, 0));
+        lblBooking.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblBooking.setText("Reservar");
+        BookingPane.add(lblBooking, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 10, 140, 40));
+
+        SideBar.add(BookingPane, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 130, 190, 60));
+
+        separator1.setBackground(new java.awt.Color(0, 0, 0));
+        separator1.setForeground(new java.awt.Color(0, 0, 0));
+        SideBar.add(separator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 190, 188, 20));
+
+        separator2.setBackground(new java.awt.Color(0, 0, 0));
+        separator2.setForeground(new java.awt.Color(0, 0, 0));
+        SideBar.add(separator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 120, 188, 20));
+
+        DashBoard.add(SideBar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 550));
 
         lblOptions1.setBackground(new java.awt.Color(255, 255, 255));
         lblOptions1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -127,7 +180,24 @@ public class mainView extends javax.swing.JFrame {
         });
         DashBoard.add(lblOptions1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 48, 48));
 
-        MainPanel.add(DashBoard, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 112, 1000, 520));
+        Events.setBackground(new java.awt.Color(255, 255, 255));
+        Events.setLayout(new java.awt.CardLayout());
+
+        pnBooking.setBackground(new java.awt.Color(255, 255, 255));
+        pnBooking.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        Events.add(pnBooking, "card3");
+
+        pnProfile.setBackground(new java.awt.Color(0, 0, 0));
+        pnProfile.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        Events.add(pnProfile, "card4");
+
+        pnSerchEvent.setBackground(new java.awt.Color(255, 255, 255));
+        pnSerchEvent.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        Events.add(pnSerchEvent, "card2");
+
+        DashBoard.add(Events, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 0, 810, 520));
+
+        MainPanel.add(DashBoard, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 110, 1010, 520));
 
         lblIcon.setBackground(new java.awt.Color(211, 82, 1));
         lblIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/images/TreeTravel.jpeg"))); // NOI18N
@@ -232,9 +302,26 @@ public class mainView extends javax.swing.JFrame {
         resetPanelColor(BookingPane);
     }//GEN-LAST:event_BookingPaneMouseExited
 
+    private void SerchPaneMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SerchPaneMouseClicked
+        pnSerchEvent.setVisible(true);
+        pnBooking.setVisible(false);
+        pnProfile.setVisible(false);
+        
+        SerchPane.setBackground(new java.awt.Color(255,100,1));
+    }//GEN-LAST:event_SerchPaneMouseClicked
+
+    private void ProfilePaneMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ProfilePaneMouseClicked
+        pnSerchEvent.setVisible(false);
+        pnBooking.setVisible(false);
+        pnProfile.setVisible(true);
+        
+        ProfilePane.setBackground(new java.awt.Color(255,100,1));
+    }//GEN-LAST:event_ProfilePaneMouseClicked
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel BookingPane;
     private javax.swing.JPanel DashBoard;
+    private javax.swing.JPanel Events;
     private javax.swing.JPanel MainPanel;
     private javax.swing.JPanel ProfilePane;
     private javax.swing.JPanel SerchPane;
@@ -243,9 +330,20 @@ public class mainView extends javax.swing.JFrame {
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JLabel lblBooking;
+    private javax.swing.JLabel lblCalendar;
     private javax.swing.JLabel lblIcon;
     private javax.swing.JLabel lblOptions;
     private javax.swing.JLabel lblOptions1;
+    private javax.swing.JLabel lblProfile;
+    private javax.swing.JLabel lblSerch;
+    private javax.swing.JLabel lblSerchEvent;
+    private javax.swing.JLabel lblUser;
+    private javax.swing.JPanel pnBooking;
+    private javax.swing.JPanel pnProfile;
+    private javax.swing.JPanel pnSerchEvent;
+    private javax.swing.JSeparator separator1;
+    private javax.swing.JSeparator separator2;
     // End of variables declaration//GEN-END:variables
 
 }
