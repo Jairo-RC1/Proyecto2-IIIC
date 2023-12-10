c9e5960a3eab6a7c9b45c5605922bdaf3b2664be
Matus <cristophermatus9@gmail.com>
2023-12-10 08:10:49 -0600

    User class + DAO role + code order + documentation

--- Proyect2_IIIC/src/model/rolDAO.java
+++ Proyect2_IIIC/src/model/rolDAO.java
@@ -1,9 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,92 +7,141 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class rolDAO {
+
     // Retrieve a list of all roles from the database
     public List<rol> readRoles() {
+        // Create a new instance of the DBConnectionJava class to establish a database connection
         DBConnectionJava db = new DBConnectionJava();
+
+        // Create an empty list to store role objects retrieved from the database
         List<rol> roles = new ArrayList<>();
+        // SQL query to select all columns from the 'roles' table
         String sql = "SELECT * FROM roles";
 
         try {
+            // Attempt to establish a connection to the database
             Connection connection = db.getConnection();
+            // Create a PreparedStatement with the SQL query
             PreparedStatement ps = connection.prepareStatement(sql);
+            // Execute the query and retrieve the result set
             ResultSet resultSet = ps.executeQuery();
+
+            // Iterate through the result set and extract role information
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
                 String name = resultSet.getString("name");
+
+                // Create a new rol object with the retrieved information and add it to the list
                 roles.add(new rol(id, name));
             }
         } catch (SQLException e) {
+            // Print an error message if any SQL exception occurs
             System.err.println("Error: " + e.getMessage());
         } finally {
+            // Disconnect from the database in the finally block to ensure resources are released
             db.disconnect();
         }
+
+        // Return the list of role objects retrieved from the database
         return roles;
     }
-    // Retrieve a role by its name
+
+    // Retrieve a role by its name from the database
     public rol getRolByName(String roleName) {
+        // Establish a new database connection
         DBConnectionJava db = new DBConnectionJava();
+        // Define the SQL query to retrieve a role by its name
         String sql = "SELECT * FROM roles WHERE name = ?";
 
         try (PreparedStatement ps = db.getConnection().prepareStatement(sql)) {
+            // Set the parameter for the role name in the prepared statement
             ps.setString(1, roleName);
+            // Execute the SQL query and retrieve the result set
             ResultSet resultSet = ps.executeQuery();
 
+            // Check if a result is found
             if (resultSet.next()) {
+                // Extract role information from the result set
                 int id = resultSet.getInt("id");
                 String name = resultSet.getString("name");
-                
+                // Return a new role object with the retrieved information
                 return new rol(id, name);
             }
         } catch (SQLException e) {
+            // Handle any SQL exception and print an error message
             System.err.println("Error: " + e.getMessage());
         } finally {
+            // Disconnect from the database in the finally block to ensure it happens even if an exception occurs
             db.disconnect();
         }
 
-        return null; // Returns null if role not found
+        // Returns null if the role is not found in the database
+        return null;
     }
-     // Retrieve the name of a role by its ID
+
+    // Retrieve the name of a role by its ID
     public String getRoleNameById(int roleId) {
+        // Establish a connection to the database
         DBConnectionJava db = new DBConnectionJava();
+
+        // SQL query to select the name of a role based on its ID
         String sql = "SELECT name FROM roles WHERE id = ?";
 
         try {
+            // Prepare a SQL statement with a parameterized query
             PreparedStatement ps = db.getConnection().prepareStatement(sql);
+            // Set the value of the parameter to the provided role ID
             ps.setInt(1, roleId);
+            // Execute the SQL query and store the result set
             ResultSet resultSet = ps.executeQuery();
 
+            // Check if there is a result in the result set
             if (resultSet.next()) {
+                // Return the name of the role found in the result set
                 return resultSet.getString("name");
             }
         } catch (SQLException e) {
+            // Print an error message if a SQL exception occurs
             System.err.println("Error: " + e.getMessage());
         } finally {
+            // Disconnect from the database regardless of the outcome
             db.disconnect();
         }
 
-        return null; // If you do not find a name corresponding to the ID
+        // Return null if no name is found corresponding to the provided ID
+        return null;
     }
+
     // Retrieve the ID of a role by its name
     public int getRoleIdByName(String roleName) {
+        // Create a new instance of the DBConnectionJava class
         DBConnectionJava db = new DBConnectionJava();
+
+        // SQL query to select the ID from the "roles" table where the name matches the provided roleName
         String sql = "SELECT id FROM roles WHERE name = ?";
 
         try {
+            // Prepare a SQL statement with the provided query
             PreparedStatement ps = db.getConnection().prepareStatement(sql);
+            // Set the parameter in the prepared statement to the provided roleName
             ps.setString(1, roleName);
+            // Execute the query and obtain the result set
             ResultSet resultSet = ps.executeQuery();
 
+            // Check if there is a result in the result set
             if (resultSet.next()) {
+                // If a result is found, return the corresponding ID
                 return resultSet.getInt("id");
             }
         } catch (SQLException e) {
+            // Print an error message if a SQL exception occurs
             System.err.println("Error: " + e.getMessage());
         } finally {
+            // Disconnect from the database in the finally block to ensure proper resource management
             db.disconnect();
         }
 
-        return -1; // You can choose a default value if no corresponding ID is found
+        // Return -1 if no corresponding ID is found (you can choose a default value)
+        return -1;
     }
-
 }
--- Proyect2_IIIC/src/model/user.java
+++ Proyect2_IIIC/src/model/user.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import java.util.Date;
 
 public class user {
 
+    // Private attributes of the user class
     private int id;
     private int idNumber;
     private String name;
@@ -17,10 +14,12 @@ public class user {
     private int phoneNumber;
     private String password;
 
+    // Default constructor with no parameters
     public user() {
 
     }
 
+    // Constructor with all attributes as parameters for initializing the user object
     public user(int id, int idNumber, String name, String lastName, Date birthDate, String email, int phoneNumber, String password) {
         this.id = id;
         this.idNumber = idNumber;
@@ -32,6 +31,7 @@ public class user {
         this.password = password;
     }
 
+    // Constructor without the 'id' parameter for scenarios where the id is generated automatically
     public user(int idNumber, String name, String lastName, Date birthDate, String email, int phoneNumber, String password) {
         this.idNumber = idNumber;
         this.name = name;
@@ -43,69 +43,83 @@ public class user {
 
     }
 
-
+    // Getter method for retrieving the id attribute
     public int getId() {
         return id;
     }
 
+    // Setter method for modifying the id attribute
     public void setId(int id) {
         this.id = id;
     }
 
+    // Getter method for retrieving the idNumber attribute
     public int getIdNumber() {
         return idNumber;
     }
 
+    // Setter method for modifying the idNumber attribute
     public void setIdNumber(int idNumber) {
         this.idNumber = idNumber;
     }
 
+    // Getter method for retrieving the name attribute
     public String getName() {
         return name;
     }
 
+    // Setter method for modifying the name attribute
     public void setName(String name) {
         this.name = name;
     }
 
+    // Getter method for retrieving the lastName attribute
     public String getLastName() {
         return lastName;
     }
 
+    // Setter method for modifying the lastName attribute
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    // Getter method for retrieving the birthDate attribute
     public Date getBirthDate() {
         return birthDate;
     }
 
+    // Setter method for modifying the birthDate attribute
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
     }
 
+    // Getter method for retrieving the email attribute
     public String getEmail() {
         return email;
     }
 
+    // Setter method for modifying the email attribute
     public void setEmail(String email) {
         this.email = email;
     }
 
+    // Getter method for retrieving the phoneNumber attribute
     public int getPhoneNumber() {
         return phoneNumber;
     }
 
+    // Setter method for modifying the phoneNumber attribute
     public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    // Getter method for retrieving the password attribute
     public String getPassword() {
         return password;
     }
 
+    // Setter method for modifying the password attribute
     public void setPassword(String password) {
         this.password = password;
     }
-
 }
