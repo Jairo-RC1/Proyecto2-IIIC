56bdf373004fdc65c1aaaaabed09763f0cf097ac
Matus <cristophermatus9@gmail.com>
2023-12-10 08:13:19 -0600

    userDAO class + documentation and organize messy code.

--- Proyect2_IIIC/src/model/userDAO.java
+++ Proyect2_IIIC/src/model/userDAO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import java.sql.PreparedStatement;
@@ -13,16 +9,23 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 public class userDAO {
+    // Method to create a new user in the database
 
     public userDAO() {
     }
-    // Method to create a new user in the database
 
+    // This method takes a user object as a parameter and inserts its information into the "users" table in the database.
     public void createUser(user user) {
+        // Establish a new database connection
         DBConnectionJava db = new DBConnectionJava();
+
+        // SQL query to insert user information into the "users" table
         String consultaSQL = "INSERT INTO users (id_number, name, last_name, email, birth_date, phone_number, password) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try {
+            // Prepare a SQL statement with the provided query
             PreparedStatement ps = db.getConnection().prepareStatement(consultaSQL);
+
+            // Set the values for each parameter in the prepared statement based on the user object
             ps.setInt(1, user.getIdNumber());
             ps.setString(2, user.getName());
             ps.setString(3, user.getLastName());
@@ -31,24 +34,39 @@ public class userDAO {
             ps.setInt(6, user.getPhoneNumber());
             ps.setString(7, user.getPassword());
 
+            // Execute the SQL statement to insert the user into the database
             ps.execute();
+
+            // Display a success message using a JOptionPane dialog
             JOptionPane.showMessageDialog(null, "Usuario insertado correctamente");
         } catch (SQLException e) {
+            // Display an error message if there is a SQL exception during the insertion process
             JOptionPane.showMessageDialog(null, "No se pudo insertar el usuario, error: " + e.getMessage());
         } finally {
+            // Disconnect from the database regardless of success or failure
             db.disconnect();
         }
     }
 
+    // This method reads user information from a database table named 'users' and returns a list of user objects.
     public List<user> readUserTxt() {
+        // Create a new instance of the DBConnectionJava class to establish a database connection
         DBConnectionJava db = new DBConnectionJava();
+
+        // Initialize an empty list to store user objects retrieved from the database
         List<user> users = new ArrayList<>();
+
+        // SQL query to select user information from the 'users' table
         String sql = "SELECT id, id_number, name, last_name, birth_date, email, phone_number, password FROM users";
 
         try {
+            // Prepare the SQL statement and execute it to get the result set
             PreparedStatement ps = db.getConnection().prepareStatement(sql);
             ResultSet resultSet = ps.executeQuery();
+
+            // Iterate through the result set to retrieve user information
             while (resultSet.next()) {
+                // Extract user attributes from the result set
                 int id = resultSet.getInt("id");
                 int idNumber = resultSet.getInt("id_number");
                 String name = resultSet.getString("name");
@@ -58,26 +76,38 @@ public class userDAO {
                 int phoneNumber = resultSet.getInt("phone_number");
                 String password = resultSet.getString("password");
 
-                // Crear y agregar un nuevo objeto user a la lista
+                // Create a new user object with the retrieved attributes
                 user newUser = new user(id, idNumber, name, lastName, birthDate, email, phoneNumber, password);
+
+                // Add the new user object to the list
                 users.add(newUser);
             }
         } catch (SQLException e) {
+            // Handle any SQL exception by printing an error message
             System.err.println("Error: " + e.getMessage());
         } finally {
+            // Disconnect from the database in the finally block to ensure proper cleanup
             db.disconnect();
         }
+
+        // Return the list of user objects
         return users;
     }
 
     // Method to update an existing user in the database
     public void updateUser(user currentUser) {
+        // Check if the user object is not null
         if (currentUser != null) {
+            // Create a new instance of the DBConnectionJava class
             DBConnectionJava db = new DBConnectionJava();
+
+            // SQL query to update user information in the 'users' table
             String consultaSQL = "UPDATE users SET id_number=?, name=?, last_name=?, birth_date=?, email=?, phone_number=?, password=? WHERE id=?";
 
             try {
+                // Establish a prepared statement with the SQL query
                 PreparedStatement ps = db.getConnection().prepareStatement(consultaSQL);
+                // Set values for each parameter in the prepared statement using data from the user object
                 ps.setInt(1, currentUser.getIdNumber());
                 ps.setString(2, currentUser.getName());
                 ps.setString(3, currentUser.getLastName());
@@ -86,32 +116,46 @@ public class userDAO {
                 ps.setInt(6, currentUser.getPhoneNumber());
                 ps.setString(7, currentUser.getPassword());
                 ps.setInt(8, currentUser.getId());
+
+                // Execute the update query
                 ps.executeUpdate();
-                JOptionPane.showMessageDialog(null, "Modificación exitosa");
+
+                // Display a success message using a JOptionPane
+                JOptionPane.showMessageDialog(null, "Modification successful");
             } catch (SQLException e) {
-                JOptionPane.showMessageDialog(null, "No se pudo modificar el usuario, error: " + e.getMessage());
+                // Display an error message if the update operation fails
+                JOptionPane.showMessageDialog(null, "Unable to modify user, error: " + e.getMessage());
             } finally {
+                // Disconnect from the database in the finally block to ensure it happens even in case of an exception
                 db.disconnect();
             }
         } else {
-            JOptionPane.showMessageDialog(null, "El usuario actual es nulo. No se puede actualizar.");
+            // Display a message if the provided user object is null
+            JOptionPane.showMessageDialog(null, "The current user is null. Cannot update.");
         }
     }
 
     // Method to delete a user from the database by ID
     public void deleteUser(int id) {
+        // Create a new instance of the DBConnectionJava class
         DBConnectionJava db = new DBConnectionJava();
-
+        // SQL query to delete a user with the specified ID from the 'users' table
         String consultaSQL = "DELETE FROM users WHERE id=?";
 
         try {
+            // Prepare a SQL statement using a PreparedStatement to prevent SQL injection
             PreparedStatement preparedStatement = db.getConnection().prepareStatement(consultaSQL);
+            // Set the value of the parameter in the SQL query to the provided user ID
             preparedStatement.setInt(1, id);
+            // Execute the SQL statement to delete the user from the database
             preparedStatement.execute();
-            JOptionPane.showMessageDialog(null, "Usuario eliminado correctamente");
+            // Display a success message using a JOptionPane dialog
+            JOptionPane.showMessageDialog(null, "User deleted successfully");
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "No se pudo eliminar el usuario, error: " + e.getMessage());
+            // Display an error message if an SQL exception occurs during the deletion process
+            JOptionPane.showMessageDialog(null, "Unable to delete user, error: " + e.getMessage());
         } finally {
+            // Ensure that the database connection is closed, regardless of success or failure
             db.disconnect();
         }
     }
